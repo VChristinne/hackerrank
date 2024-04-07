5ce5808448be37f55ff0eeee56161a807c8c5275
Christinne <vanessa0christinne@gmail.com>
2024-04-07 17:15:20 +0200

    Solution for Access Denied

--- cpp/access_denied.cpp
+++ cpp/access_denied.cpp
@@ -4,7 +4,7 @@
 <H1>Access Denied</H1>
  
 You don't have permission to access "http&#58;&#47;&#47;www&#46;hackerrank&#46;com&#47;challenges&#47;cpp&#45;hello&#45;world&#47;submissions&#47;code&#47;377816766" on this server.<P>
-Reference&#32;&#35;18&#46;c551060&#46;1712502546&#46;69fec966
-<P>https&#58;&#47;&#47;errors&#46;edgesuite&#46;net&#47;18&#46;c551060&#46;1712502546&#46;69fec966</P>
+Reference&#32;&#35;18&#46;c551060&#46;1712502920&#46;6a085914
+<P>https&#58;&#47;&#47;errors&#46;edgesuite&#46;net&#47;18&#46;c551060&#46;1712502920&#46;6a085914</P>
 </BODY>
 </HTML>
